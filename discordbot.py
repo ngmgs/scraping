e665782dbc0654ebd657e18b564e1c9604999b4a
ngmgs <112070929+ngmgs@users.noreply.github.com>
2022-09-02 22:33:09 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,7 +18,7 @@ def main():
     #print(url)
     for item in soup.find_all(class_="dui-card searchresultitem"):
         print("#" * 50)
-        print(soup.find(class_="important").text)
+        print(item.soup.find("a").text)
         #print(item)
         print(item.text)
         title = item.get("title")
