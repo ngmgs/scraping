5971b6d62ee462cb4bdbae5d5e005f2445bee2be
ngmgs <112070929+ngmgs@users.noreply.github.com>
2022-09-03 12:05:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,10 +16,10 @@ def main():
         "https://search.rakuten.co.jp/search/mall/%E3%83%9F%E3%83%83%E3%82%AF%E3%82%B9%E3%83%8A%E3%83%83%E3%83%84/").content
     soup = BeautifulSoup(url)
     #print(url)
-    for item in soup.find_all("a", attrs={"data-track-trigger": "title", "target": "_top"}):
+    for item in soup.find_all(class_="dui-card searchresultitem"):
         print("#" * 50)
-        print(item.text)
-        #print(item.soup.find(class_="important").text)
+        #print(item.text)
+        print(item.find(class_="important").text)
 
 
 
