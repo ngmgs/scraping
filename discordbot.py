b5809314d320bd72f63f6a35a8ded88120a56f6c
ngmgs <112070929+ngmgs@users.noreply.github.com>
2022-09-07 13:37:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,40 +19,26 @@ is_matched = {}
 is_text = {}
 
 
+# 送信された時間を代入する変数を各関数でグローバル宣言する
+send_time = None
+
 async def _check_url(message: discord.Message):
-    if message.author.bot:
-        return
-    # メッセージからURLを抽出
-    url_list = re.findall(pattern, message.content)
+    global send_time
     # もしメッセージにURLが含まれていたら
-    if url_list:
-        print("1時間以内に同じURLを送信したら削除する")
-        # もし辞書にURLが登録されていたら(含まれていなかったらNoneが返る)
-        if is_text.get(url_list[0], None) is not None:
-            # 送信されていた時間を取り出す
-            _sent_date = is_text[url_list[0]]
-            print("辞書に登録されている発言時間は" + str(_sent_date))
-            # もし差分が3600秒以上(1h)なら
-            if (datetime.datetime.now() - _sent_date).seconds >= 3600:
-                # 辞書のURLが持つ発言時間を更新して終了
-                print("辞書のURL(" + url_list[0] + ")が持つ発言時間を更新")
-                is_text[url_list[0]] = datetime.datetime.now()
-                return
-            else:
-                # 1h以内に投稿されていた場合削除
-                print(url_list[0])
-                print("そのURL(" + url_list[0] + ")が入ったメッセージが1時間以内に投稿されています。削除します。")
-                alert_msg = await message.channel.send("そのURLが入ったメッセージが1時間以内に投稿されています。削除します。")
-                await message.delete(delay=1)
-                await alert_msg.delete(delay=3)
-                return
-        else:
-            # 辞書にURLが登録されていなかったのでURLと発言時間を登録する
-            print("辞書にURLと発言時間を登録")
-            is_text[url_list[0]] = datetime.datetime.now()
-            print(is_text)
-    else:
-        print("メッセージにURLはない")
+    if url.search(message.content) is not None:
+        if send_time is not None:
+	    _now = datetime.datetime.now()
+				
+	    # メッセージが1時間以内に送信されていたら		        
+	    if (_now - send_time).seconds < 3600:
+	        alert_msg = await message.channel.send("そのURLが入ったメッセージが1時間以内に投稿されています。削除します。")
+	        # 送ったメッセージとともに削除
+	        await message.delete(delay=1)
+	        await alert_msg.delete(delay=3)
+	
+	    else:
+            # send_timeを更新
+            send_time = _now
 
 '''
 async def _check_url(message: discord.Message):
