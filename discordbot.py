0ad783da9e8e446ee740b256f1e5b97fc632b598
ngmgs <112070929+ngmgs@users.noreply.github.com>
2022-09-15 13:51:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,6 +8,8 @@ from datetime import datetime, timezone, timedelta, time
 from bs4 import BeautifulSoup
 
 bot = commands.Bot(command_prefix="/",intents=discord.Intents.all())
+is_pc4u_amd = {}
+is_pc4u_nvidia = {}
 
 @tasks.loop(minutes=1)
 async def send_message_every():
@@ -16,6 +18,7 @@ async def send_message_every():
     JST = timezone(t_delta, 'JST')
     now = datetime.now(JST).strftime('%A/%H:%M')
     await pc4u_amd()
+    await pc4u_nvidia()
 
 
 @bot.event
@@ -23,8 +26,8 @@ async def on_ready():
     send_message_every.start()
 
 
-is_pc4u_amd = {}
-#PC4Uからグラボの商品名と価格を取得
+
+#PC4Uからamdグラボの商品名と価格を取得
 async def pc4u_amd():
     channel_sent = bot.get_channel(1019194136349392916)
     url = requests.get(
@@ -76,6 +79,58 @@ async def pc4u_amd():
             continue
     #print(is_pc4u_amd)
 
+#PC4Uからnvidiaグラボの商品名と価格を取得
+async def pc4u_nvidia():
+    channel_sent = bot.get_channel(1019194136349392916)
+    url = requests.get(
+        "https://www.pc4u.co.jp/shopbrand/ct1850/page1/price/").content
+    soup = BeautifulSoup(url)
+    #print(url)
+    # await channel_sent.send("pc4u")
+    for item in soup.find_all(class_="innerBox"): #商品の親要素divをクラス名で取得
+        title = item.find(class_="name").text #itemからクラス名で商品名を取得
+        price = item.find(class_="price").text #itemからクラス名で価格を取得
+        url_temp = item.find('a') #itemからクラス名で価格を取得
+        url = "https://www.pc4u.co.jp" + url_temp.get('href')
+
+
+        # もし辞書に商品が登録されていたら(含まれていなかったらNoneが返る)
+        if is_pc4u_nvidia.get(title, None) is not None:
+            # 辞書に登録されている価格を取得
+            _sent_price = is_pc4u_nvidia[title]
+            if price != _sent_price:
+                is_pc4u_nvidia[title] = price
+                print("#" * 50)
+                print("価格が変更!!")
+                print(title)
+                print(is_pc4u_nvidia[title])
+                print(url)
+                await channel_sent.send(title)
+                await channel_sent.send(is_pc4u_nvidia[title])
+                await channel_sent.send(url)
+            # 価格が同じ場合
+            '''
+            else:
+                print("価格に変更はない")
+                print(title)
+                print(is_pc4u_nvidia[title])
+                print(url)
+            '''
+        # 辞書に商品が登録されていなかったので価格を登録する
+        else:
+            is_pc4u_nvidia[title] = price
+            # もし辞書と現在の価格が違えば更新
+            print("#" * 50)
+            print("初回登録")
+            print(title)
+            print(is_pc4u_nvidia[title])
+            print(url)
+
+
+        if title is None:
+            continue
+    #print(is_pc4u_nvidia)
+
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -87,6 +142,7 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await pc4u_amd()
+    await pc4u_nvidia()
 
 
 token = getenv('DISCORD_BOT_TOKEN')
