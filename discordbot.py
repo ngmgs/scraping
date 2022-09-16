da0f2fbfd8e942e17447adddd36316606cc337f8
ngmgs <112070929+ngmgs@users.noreply.github.com>
2022-09-16 21:25:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,6 +9,17 @@ from datetime import datetime, timezone, timedelta, time
 from bs4 import BeautifulSoup
 
 bot = commands.Bot(command_prefix="/",intents=discord.Intents.all())
+
+async def pc4u_test(url):
+    print(url)
+
+@bot.command()
+async def ping(ctx):
+    temp = 'ぷりん'
+    await pc4u_test(temp)
+
+
+'''
 is_pc4u_amd = {}
 is_pc4u_nvidia = {}
 
@@ -176,7 +187,7 @@ async def ping(ctx):
     await pc4u_amd()
     t.sleep(5)
     await pc4u_nvidia()
-
+'''
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
