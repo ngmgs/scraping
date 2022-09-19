085d70ae6d1cdb68e0fea354e9dde2fb5fc5946e
ngmgs <112070929+ngmgs@users.noreply.github.com>
2022-09-19 20:39:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -27,8 +27,8 @@ async def main():
     JST = timezone(t_delta, 'JST')
     now = datetime.now(JST).strftime('%A/%H:%M:%S')
     print(now)
-    result = anser = [keisan(i) for i in range(1,11)]
-    await asyncio.gather(*anser)
+    anser = [keisan(i) for i in range(1,11)]
+    result = await asyncio.gather(*anser)
     print(datetime.now(JST).strftime('%A/%H:%M:%S'))
     print(result)
     
