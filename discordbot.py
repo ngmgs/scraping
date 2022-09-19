92e6b853e2304e9489c95c61370b956ac609d6c2
ngmgs <112070929+ngmgs@users.noreply.github.com>
2022-09-19 22:49:52 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,8 +5,6 @@ import asyncio
 import aiohttp
 import async_timeout
 import time as t
-import csv
-import os
 import json
 from discord.ext import commands
 from discord.ext import tasks
@@ -14,80 +12,29 @@ from os import getenv
 from datetime import datetime, timezone, timedelta, time
 from bs4 import BeautifulSoup
 
-bot = commands.Bot(command_prefix="^",intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="/",intents=discord.Intents.all())
 
 
 is_pc4u_amd = {}
 is_pc4u_nvidia = {}
 
 
-
 async def main():
-    t_delta = timedelta(hours=9)
-    JST = timezone(t_delta, 'JST')
-    now = datetime.now(JST).strftime('%A/%H:%M:%S')
-    print(now)
-    anser = [keisan(i) for i in range(1,11)]
-    result = await asyncio.gather(*anser)
-    print(datetime.now(JST).strftime('%A/%H:%M:%S'))
-    print(result)
-    print(result[9])
-    print(result[9][1])
     
-async def sub():
-    t_delta = timedelta(hours=9)
-    JST = timezone(t_delta, 'JST')
-    now = datetime.now(JST).strftime('%A/%H:%M:%S')
-    print(now)
-    for i in range(1,11):
-        await keisan(i)
-    print(datetime.now(JST).strftime('%A/%H:%M:%S'))
-    
-async def tes():
-    t_delta = timedelta(hours=9)
-    JST = timezone(t_delta, 'JST')
-    now = datetime.now(JST).strftime('%A/%H:%M:%S')
-    print(now)
-    anser = [keisan(i) for i in range(1,11)]
-    await asyncio.gather(anser)
-    print(datetime.now(JST).strftime('%A/%H:%M:%S'))
-    print(anser)
-    
-async def keisan(i):
-    tashizan = i + i
-    await asyncio.sleep(1)
-    kakezan = i * i
-    return tashizan, kakezan
- 
-@bot.command()
-async def mm(ctx):
-    url = "https://www.pc4u.co.jp/shopbrand/pciexpress4/page1/price/"
-    await main()
-    
-@bot.command()
-async def ss(ctx):
-    url = "https://www.pc4u.co.jp/shopbrand/pciexpress4/page1/price/"
-    await sub()
-    
-@bot.command()
-async def tt(ctx):
-    channel_sent = bot.get_channel(1012237139729199136)
-    title = "gtx600"
-    price = "300"
-    await channel_sent.send(title + "\n" + price)
-    
-    
-asyncio.run(main())
-"""
-        
-with open('is_pc4u_amd.csv', 'w') as f:  
-    writer = csv.writer(f)            
-    for k, v in is_pc4u_amd.items():
-       writer.writerow([k, v])
+    async with aiohttp.ClientSession() as session:
+        urls = {
+            'https://www.pc4u.co.jp/shopbrand/pciexpress4/page1/price/': is_pc4u_amd,
+            # 'https://www.pc4u.co.jp/shopbrand/ct1850/page1/price/': is_nvidia,
+        }
+        promises = [fetch(session, url, dic) for url, dic in urls.items()]
+        print(promises)
+        await asyncio.gather(*promises)
 
-        
+
 async def fetch(session, url, dic):
     print("{} start".format(url))
+
+
     while True:
         async with async_timeout.timeout(100):
             async with session.get(url) as response:
@@ -95,11 +42,12 @@ async def fetch(session, url, dic):
                 soup = BeautifulSoup(html, "html.parser")
 
                 # もし辞書が空の時（再起動等で辞書が空のとき）
-                '''
-                promises = [first_items(item, dic) for item in soup.find_all(class_="innerBox")]
-                await asyncio.gather(*promises)
-                print("辞書に全アイテム登録完了")
-                '''
+
+                if any(dic) == False:
+                    promises = [first_items(item, dic) for item in soup.find_all(class_="innerBox")]
+                    await asyncio.gather(*promises)
+                    print("辞書に全アイテム登録完了")
+                    return
 
                 promises = [get_items(item, dic) for item in soup.find_all(class_="innerBox")]
                 await asyncio.gather(*promises)
@@ -115,21 +63,33 @@ async def fetch(session, url, dic):
     return
 
 async def first_items(item, dic):
-    
-    title = item.select_one('p.name').text  # itemからクラス名で商品名を取得
-    price = item.select_one('p.price').text  # itemからクラス名で価格を取得
-    stock = item.select_one('div.btnWrap > img')  #itemからクラス名で品切れ情報を取得
-    
-    dic[title] = {'price': price, 'stock': stock}
+    while True:      
 
-    
+        title = item.select_one('p.name').text  # itemからクラス名で商品名を取得
+        price = item.select_one('p.price').text  # itemからクラス名で価格を取得
+        stock = item.select_one('div.btnWrap > img')  #itemからクラス名で品切れ情報を取得
+
+        dic[title] = {'price': price, 'stock': stock}
+
+        url = await next_page(session, soup)
+
+        if url is None:
+            break
+
+        await asyncio.sleep(5)
+
+        async with session.get(url) as response:
+            html = await response.text()
+            item = BeautifulSoup(html, "html.parser")
+
+
 
 async def get_items(item, dic):
     channel_sent = bot.get_channel(1019194136349392916)
     title = item.select_one('p.name').text  # itemからクラス名で商品名を取得
     price = item.select_one('p.price').text  # itemからクラス名で価格を取得
     stock = item.select_one('div.btnWrap > img')  #itemからクラス名で品切れ情報を取得
-    
+
 
     # もし辞書に商品が登録されていたら(含まれていなかったらNoneが返る)
     if dic.get(title, None) is not None:
@@ -158,14 +118,14 @@ async def get_items(item, dic):
     # 辞書に商品が登録されていなかったので価格を登録する
     else:            
         dic[title] = {'price': price, 'stock': stock}
-        '''
+
         print("新規登録")
 
         await channel_sent.send("新規登録")
         await channel_sent.send(title)
         await channel_sent.send(dic[title]['price'])
         await channel_sent.send(url)
-        '''
+
 
 
 
@@ -180,225 +140,11 @@ async def next_page(session, soup):
     print("次のページは")
     print(url)
     return url
-    
 
 
-              
-'''     
-        async with session.get(url) as response:
-            html = await response.text()
-            soup = BeautifulSoup(html, "html.parser")
-            
-            try:
-                url_next = soup.select_one('li.next > a[href]:-soup-contains("次の50件")').get('href')
-            except AttributeError:
-                print("最後のページです")
-            print(url_next)
-            print(url)
-    
-
-    async with aiohttp.ClientSession() as session:
-
-        async with session.get(url) as res:
-
-            soup = BeautifulSoup(res, 'html.parser')
-            try:
-                url_next = soup.select_one('li.next > a[href]:-soup-contains("次の50件")').get('href')
-            except AttributeError:
-                print("最後のページです")
-            print(url_next)
-        
-'''
-
-    
-
-@tasks.loop(minutes=1)
-async def send_message_every():
-    channel_sent = bot.get_channel(1019194136349392916)
-    t_delta = timedelta(hours=9)
-    JST = timezone(t_delta, 'JST')
-    now = datetime.now(JST).strftime('%A/%H:%M')
-    url = "https://www.pc4u.co.jp/shopbrand/pciexpress4/page1/price/"
-    print(now)
-    await pc4u_get_vga(url, is_pc4u_amd)
-    print(list(is_pc4u_amd.items())[0])
-    await asyncio.sleep(5)
-    url = "https://www.pc4u.co.jp/shopbrand/ct1850/page1/price/"
-    await pc4u_get_vga(url, is_pc4u_nvidia)
-    print(list(is_pc4u_nvidia.items())[0])
-    
-    # await pc4u_nvidia()
-
-
-@bot.event
-async def on_ready():
-    send_message_every.start()  # ループ処理開始
-
-
-
-#PC4Uからamdグラボの商品名と価格を取得
-async def pc4u_get_vga(url, is_pc4u):
-    channel_sent = bot.get_channel(1019194136349392916)
-    session = requests.Session()
-    res = session.get(url).content
-    soup = BeautifulSoup(res, 'html.parser')
-    # await channel_sent.send("pc4u")
-    for item in soup.find_all(class_="innerBox"): #商品の親要素divをクラス名で取得
-        title = item.find(class_="name").text #itemからクラス名で商品名を取得
-        price = item.find(class_="price").text #itemからクラス名で価格を取得
-        stock = item.find(class_="btnWrap").find('img') #itemからクラス名で品切れ情報を取得
-        url_temp = item.find('a') #itemからクラス名で価格を取得
-        url = "https://www.pc4u.co.jp" + url_temp.get('href')
-
-
-        # もし辞書に商品が登録されていたら(含まれていなかったらNoneが返る)
-        if is_pc4u.get(title, None) is not None:
-            # 辞書に登録されている価格を取得
-            _sent_price = is_pc4u[title]['price']
-            # もし辞書と現在の価格が違えば更新
-            if price != _sent_price:
-                is_pc4u[title]['price'] = price
-                print("#" * 50)
-                print("価格が変更!!")
-                print(title)
-                print(is_pc4u[title])
-                print(url)
-                await channel_sent.send(title)
-                await channel_sent.send(is_pc4u[title])
-                await channel_sent.send(url)
-            # 価格が同じ場合
-            '''
-            else:
-                print("価格に変更はない")
-                print(title)
-                print(is_pc4u[title])
-                print(url)
-            '''
-        # 辞書に商品が登録されていなかったので価格を登録する
-        else:            
-            is_pc4u[title] = {'price': price, 'stock': stock}
-
-            '''
-            print("#" * 50)
-            print("初回登録")
-            print(title)
-            print(is_pc4u[title])
-            print(url)
-            print(stock)
-            '''
-
-        if title is None:
-            continue
-    #print(is_pc4u)
-    #return is_pc4u
-
-    while True:
-        print(session)
-        try:
-            url_next = soup.select_one('li.next > a[href]:-soup-contains("次の50件")').get('href')
-        except AttributeError:
-            print("最後のページです")
-            break
-        url_next = soup.select_one('li.next > a[href]:-soup-contains("次の50件")').get('href')
-        url = "https://www.pc4u.co.jp" + url_next
-        print("次のページは")
-        print(url)
-        await asyncio.sleep(5)
-        res = session.get(url).content
-        soup = BeautifulSoup(res, 'html.parser')
-    print("ブレイクしたよ")
-'''
-    t.sleep(5)
-    res = requests.get(url).content
-    soup = BeautifulSoup(res, 'html.parser')
-    url_next = soup.select_one('li.next > a[href]:-soup-contains("次の50件")').get('href')
-    url = "https://www.pc4u.co.jp" + url_next
-    print(url)
-    if url_next is None:
-        print("2:次のページはない")
-    else:
-        print("2:次のページある")
-
-    t.sleep(5)
-    res = requests.get(url).content
-    soup = BeautifulSoup(res, 'html.parser')
-    try:
-        url_next = soup.select_one('li.next > a[href]:-soup-contains("次の50件")').get('href')
-    except AttributeError:
-        print("最後のページです")
-        return
-    url = "https://www.pc4u.co.jp" + url_next
-    print(url)
-    if url_next is None:
-        print("3次のページはない")
-    else:
-        print("3:次のページある")
-'''
-        
-
-#PC4Uからnvidiaグラボの商品名と価格を取得
-async def pc4u_nvidia():
-    channel_sent = bot.get_channel(1019194136349392916)
-    url = requests.get(
-        "https://www.pc4u.co.jp/shopbrand/ct1850/page1/price/").content
-    soup = BeautifulSoup(url, 'html.parser')
-    #print(url)
-    # await channel_sent.send("pc4u")
-    for item in soup.find_all(class_="innerBox"): #商品の親要素divをクラス名で取得
-        title = item.find(class_="name").text #itemからクラス名で商品名を取得
-        price = item.find(class_="price").text #itemからクラス名で価格を取得
-        url_temp = item.find('a') #itemからクラス名で価格を取得
-        url = "https://www.pc4u.co.jp" + url_temp.get('href')
-
-
-        # もし辞書に商品が登録されていたら(含まれていなかったらNoneが返る)
-        if is_pc4u_nvidia.get(title, None) is not None:
-            # 辞書に登録されている価格を取得
-            _sent_price = is_pc4u_nvidia[title]
-            # もし辞書と現在の価格が違えば更新
-            if price != _sent_price:
-                is_pc4u_nvidia[title] = price
-                print("#" * 50)
-                print("価格が変更!!")
-                print(title)
-                print(is_pc4u_nvidia[title])
-                print(url)
-                await channel_sent.send(title)
-                await channel_sent.send(is_pc4u_nvidia[title])
-                await channel_sent.send(url)
-            # 価格が同じ場合
-            '''
-            else:
-                print("価格に変更はない")
-                print(title)
-                print(is_pc4u_nvidia[title])
-                print(url)
-            '''
-        # 辞書に商品が登録されていなかったので価格を登録する
-        else:
-            is_pc4u_nvidia[title] = price
-            '''
-            print("#" * 50)
-            print("初回登録")
-            print(title)
-            print(is_pc4u_nvidia[title])
-            print(url)
-            '''
-
-
-        if title is None:
-            continue
-    #print(is_pc4u_nvidia)
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-"""
-
-
+@bot.command()
+async def ping(ctx):
+    await main()
 
 @bot.command()
 async def pank(ctx):
